6420d781496aaf3439507e6498a7e0a6c6afade5
Jenny <gerelmaapurevtseren@gmail.com>
2020-11-13 08:05:18 -0800

    new lesson

--- assignments/week3/zoo.js
+++ assignments/week3/zoo.js
@@ -1,95 +1,119 @@
-class Animal{
-    constructor(alive){
-        this.living=alive;
+class Animal {
+    constructor(alive) {
+        this.living = alive;
+    }
+
+    died() {
+        this.living = false;
     }
 }
 
-class Mammal extends Animal{
-    constructor(alive,eyeColor) {
-        super();
-        this.tail=1;
-        this.type="mammal";
-        this.eyes=2;
-        this.eyeColor=blue;
+class Mammal extends Animal {
+    constructor(alive, eyeColor) {
+        super(alive);
+        this.tail = 1;
+        this.type = "mammal";
+        this.eyes = 2;
+        this.eyeColor = eyeColor;
     }
+
     eat() {
-if("name" in this){
-
-        console.log(this.name + " eats some food.");
-} else {
-    console.log("Animal eats some food");
+        if ("name" in this) {
+            console.log(this.name + " eats some food.");
+        } else {
+            console.log("Animal eats some food");
+        }
     }
-    }
-    breed(partner){
-        if(partner.constructor.name== this.constructor.name){
-            console.log("Good job");
 
-            const eyeInheritance=Math.random()*2;
-            eyeInheritance=Math.floor(eyeInheritance);
-            if(eyeInheritance==0){
-                childEyeColor=this.eyeColor;
-            } else{
-                childEyeColor=partner.eyeColor
+    breed(partner, childName) {
+        if (partner.constructor.name == this.constructor.name) {
+            console.log("successful breeding");
+            
+            let eyeInheritance = Math.random() * 2;
+            eyeInheritance = Math.floor(eyeInheritance);
 
+            let childEyeColor;
+
+            if (eyeInheritance == 0) {
+                childEyeColor = this.eyeColor;
+            } else {
+                childEyeColor = partner.eyeColor;
             }
 
-            let child=new this.constructor(childName,true,childEyeColor);
-            console.log(child);
-
+            let child = new this.constructor(childName, true, childEyeColor);
+            return child;
         }
-        return child;
     }
 }
-class Dog extends Mammal{
-    constructor(dogName,alive){
-        //This function runs the constructor of the class we are inheriting,it is important to run it first before any other lines of our dog constructor class.
-        super(alive);
-        this.legs=4;
-        this.name=dogName;
-        this.eyeColor;
+
+class Dog extends Mammal {
+    constructor(dogName, alive, eyeColor) {
+        // This function runs the constructor of the class we are inheriting, it is important to run it first before any other lines of our Dog constructor class.
+        super(alive, eyeColor);
+        this.legs = 4;
+        this.name = dogName;
+
     }
-    bark(){
-        console.log("woof woof");
-    }
-}
-class Cat extends Mammal{
-    constructor(catName,alive){
-        super(alive);
-        this.legs=4;
-        this.name=catName;
-    }
-    meow(){
-        console.log("meow");
+
+    bark() {
+        console.log("woof woof!");
     }
 }
-let animal1 =new Dog ("Banhar",true);
-let animal2 =new Cat ("Boroo",true);
-animal1.bark();
-animal1.eat();
-console.log(animal1.living);
-animal2.eat();
-animal2.meow();
-console.log(animal2.living);
 
-let dog1=new Dog("Banhar",true,"blue");
-let dog2=new Dog("Shonhor",true,"brown");
-let dog3=new Dog("Tuna",true,"green");
+class Cat extends Mammal {
+    constructor(catName, alive, eyeColor) {
+        super(alive, eyeColor);
+        this.legs = 4;
+        this.name = catName;
+    }
 
-let newDog=dog2.breed(dog1,"Burged");
-//let newDog=dog3.breed(dog2,"Irwes")
+    meow() {
+        console.log("meow!");
+    }
+}
 
-console.log(isNaN());
-Math.random();
-Math.round();
-let myFirstNumber=new Number("10");
-let number1=19;
-console.log(myFirstNumber,number1);
+let dog1 = new Dog("Fido", true, "brown");
+let dog2 = new Dog("Fluffy", true, "green");
+let dog3 = new Dog("Pluto", true, "amber");
 
-let cat1=new Cat("sue",true,"blue");
-let cat2=new Cat("cici",true,"brown");
+let newDog = dog2.breed(dog1, "Spot");
+let newDog2 = dog3.breed(dog2, "Fluffy Jr.");
 
-let newcat1=cat1.breed(cat2,"Spot");
+console.log(newDog);
+console.log(newDog2);
+
+let cat1 = new Cat("Coco", true, "aqua");
+let cat2 = new Cat("Lucy", true, "amber");
+
+let newCat1 = cat1.breed(cat2, "Scratchy");
 
 console.log(newCat1);
 
 
+
+
+// console.log(isNaN());
+
+// Math.random();
+// Math.round();
+// let myFirstNumber = new Number("10");
+// let number1 = 19;
+
+// console.log(myFirstNumber, number1);
+
+
+
+
+
+// let animal1 = new Dog("Fido", true);
+// let animal2 = new Cat("Garfield", true);
+
+// animal1.bark();
+// animal1.eat();
+// console.log(animal1.living);
+// animal2.eat();
+// animal2.meow();
+// console.log(animal2.living);
+
+// animal1.died();
+// console.log(animal1.living);
