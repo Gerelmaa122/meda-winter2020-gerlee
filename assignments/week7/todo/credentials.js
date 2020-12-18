80f14a93be786d534bb3821af4e55b01652e4bda
Jenny <gerelmaapurevtseren@gmail.com>
2020-12-17 19:48:21 -0800

    react

--- assignments/week7/todo/credentials.js
+++ assignments/week7/todo/credentials.js
@@ -1,4 +1,4 @@
-const databaseAccess="mongodb+srv://todoUser:todoUser@cluster0.ncwph.mongodb.net/noteCollections?retryWrites=true&w=majority";
+const databaseAccess="mongodb+srv://todoUser:todoUser@cluster0.ncwph.mongodb.net/todoApp?retryWrites=true&w=majority";
 
 const secretValue=1000;
 module.exports={
